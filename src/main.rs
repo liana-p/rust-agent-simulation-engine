6dec64ccfe0b73453b3fd6664cf503bc945277e4
Liana Pigeot <32243840+nialna@users.noreply.github.com>
2021-07-24 12:50:31 +0100

    trying to get system queries to work

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,47 @@ struct TagState {
 
 
 struct TagAgent{
+    pub position: Position,
     pub is_it: bool,
+    pub systems: Vec<String>,
 }
-impl AgentState for TagAgent{}
+impl AgentState for TagAgent{
+    fn systems(&self) -> Vec<String> {
+        return self.systems;
+    }
+}
+
+#[derive(Default)]
 struct TagAgentSystem {
 
 }
 
 impl System for TagAgentSystem {
-
-    fn simulate(agent: &mut Agent, state: &mut TagAgent) {
-        agent.position.x += 1f32;
-        state.is_it = true;
+    type StateData = StateStorage<TagAgent>;
+    fn simulate(&self, data: &mut Self::StateData) {
+        for agent in &mut data.agents {
+            agent.position.x += 1f32;   
+        }
     }
 }
+
+type Systems = TagAgentSystem;
+type Agents = TagAgent;
+
 fn main() {
     println!("Hello, world!");
-    let mut world = World {
-        agents: vec![],
-        systems: vec![],
+    let world: World<Systems, Agents> = World {
+        agents: Vec::new(),
+        systems: Vec::new(),
     };
-    world.add_agent(Box::new(TagAgent {
+    let agent = TagAgent {
         is_it: true,
-    }));
+        position: Position {
+            x: 0f32,
+            y: 0f32,
+        },
+    };
+    world.add_agent(agent)
+    .add_system(TagAgentSystem::default(), String::from("Tag Agent System"));
+
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -13,31 +13,52 @@ pub mod simulation {
         pub state: Box<dyn AgentState>,
     }
 
-    pub trait AgentState {}
-
-    pub struct World {
-        pub agents: Vec<Agent>,
-        pub systems: Vec<dyn System>,
+    pub trait AgentState {
+        fn systems(&self) -> Vec<String>;
     }
 
-    impl World {
-        pub fn add_agent(&mut self, state: Box<dyn AgentState>) {
-            self.agents.push(Agent {
-                id: Uuid::new_v4().to_string(),
-                position: Position {
-                    x: 0f32,
-                    y: 0f32,
-                },
-                state: state,
+    pub struct StateStorage<T> {
+        pub agents: Vec<T>,
+    }
+    pub struct World<S: System, A: AgentState> {
+        pub agents: Vec<Box<A>>,
+        pub systems: Vec<Box<SystemStorage<S>>>,
+    }
+
+    pub struct SystemStorage<S: System> {
+        pub system: S,
+        pub name: String,
+    }
+
+    impl<S, A> World<S, A>
+    where 
+        S: System,
+        A: AgentState,
+    {
+        pub fn add_agent(mut self, state: A) -> Self{
+            self.agents.push(Box::new(state));
+            return self;
+        }
+
+        pub fn add_system(mut self, system: S, name: String) -> Self {
+            let storage = Box::new(SystemStorage {
+                system,
+                name,
             });
+            self.systems.push(storage);
+            return self;
         }
 
-        pub fn tick() {
-            
+        pub fn run_systems(mut self) -> Self {
+            for system in &self.systems {
+                // TODO: Figure out how to query agents to create a SystemStorage based on system data type
+            }
+            return self;
         }
     }
+
     pub trait System {
         type StateData;
-        fn simulate(&self, agent: &mut Agent, state: &mut impl AgentState);
+        fn simulate(&self, state: &mut Self::StateData);
     }
 }
